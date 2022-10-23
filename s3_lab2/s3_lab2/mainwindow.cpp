2090088edb84050e9b3a722413b6306e37809420
Dm <dmytromk@gmail.com>
2022-10-23 20:15:47 +0300

    Initial menu

--- s3_lab2/s3_lab2/mainwindow.cpp
+++ s3_lab2/s3_lab2/mainwindow.cpp
@@ -6,10 +6,25 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    probSetWindow = new ProbabilitySetWindow();
+    connect(probSetWindow, &ProbabilitySetWindow::mainMenuWindow,
+            this, &MainWindow::show);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_probabilitySetButton_clicked()
+{
+    probSetWindow->show();
+    this->close();
+}
+
+void MainWindow::on_quitButton_clicked()
+{
+    this->close();
 }
 
--- s3_lab2/s3_lab2/mainwindow.h
+++ s3_lab2/s3_lab2/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "probabilitysetwindow.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_probabilitySetButton_clicked();
+
+    void on_quitButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    ProbabilitySetWindow *probSetWindow;
 };
 #endif // MAINWINDOW_H
