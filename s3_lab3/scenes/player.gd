f73ba071011b0819297dae004155263b8b55a391
Dm <dmytromk@gmail.com>
2022-12-03 23:15:40 +0200

    fix: changed player speed, velocity, etc. to match the new resolution

--- s3_lab3/scenes/player.gd
+++ s3_lab3/scenes/player.gd
@@ -1,14 +1,13 @@
 extends KinematicBody2D
 
-export var jump_impulse : float = 400
-export var gravity_impulse : float = 10
-export var speed : float = 3.0 * 60
+export var jump_impulse : float = 5.5 * 60
+export var gravity_impulse : float = 7.0 * 60
+export var speed : float = 2 * 60
 
 var score = 0 #also traveled y-distance
 
 var velocity = Vector2.ZERO
 
-onready var camera = get_node('../Camera2D')
 var game_over = false
 
 func _physics_process(delta):
@@ -24,7 +23,7 @@ func _physics_process(delta):
 	velocity.x = lerp(velocity.x, 0, 0.2) #smoothes transition between "moving" and "stopping" (key is not pressed)
 	
 	if is_on_floor():
-		velocity += (Vector2.UP * jump_impulse)
+		velocity.y = -jump_impulse
 		
 	move_and_slide(velocity, Vector2.UP)
 	
@@ -34,10 +33,3 @@ func _physics_process(delta):
 	elif position.x > get_viewport_rect().size.x:
 		position.x = 0
 		
-	# score system
-	if position.y < score:
-		score = position.y
-		camera.position.y = position.y
-	
-	if (-score) - (-position.y) > 0:
-		game_over = true
